a7b55a901c0f001e8a303c8a0f42487aaca20843
Katey VanKerkhove <kateyvankerkhove1@gmail.com>
2022-07-11 11:54:33 -0400

    start carousel for crops page

--- client/src/components/Crops.js
+++ client/src/components/Crops.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import './Crops.css'
+// import './Crops.css'
+import './Crops.scss'
 // import escarole from '../images/escarole.png'
 
 function Crops() {
@@ -51,10 +52,62 @@ function Crops() {
       )
     })
     // console.log(renderCropsInFamily)
+    // let firstCropId = `slide-${cropsInFamily[0].id}`
+    const renderRadioInputs = cropsInFamily.slice(1).map(crop => {
+      let inputId = `slide-${cropsInFamily.indexOf(crop) + 1}`
+
+      // console.log(i)
+      // let cropId = `slide-${crop.id}`
+      return <input type="radio" name="slides" id={inputId}></input>
+    })
+
+    const renderCarouselSlides = cropsInFamily.map(crop => {
+      return (
+        <li className="carousel__slide">
+          <figure>
+            <div>
+              <img src={ require(`../images/${crop.image}`)} alt=""/>
+            </div>
+            <figcaption>
+              Details: {crop.details}
+            </figcaption>
+          </figure>
+        </li>
+      )
+    })
+
+    console.log(cropsInFamily)
+
+    const renderCarouselThumbnails = cropsInFamily.map(crop => {
+      let inputId = `slide-${cropsInFamily.indexOf(crop) + 1}`
+      
+
+      return (
+        <li>
+          <label htmlFor={inputId}><img src={ require(`../images/${crop.image}`)} alt=""/></label>
+        </li>
+      )
+    })
+
+    // console.log(renderCarouselThumbnails)
+
     return (
-      <div id={id} class="page">
-        {renderCropsInFamily}
-      </div>
+      <section>
+        <div id={id} className="page container">
+          <div className="carousel">
+            <input type="radio" name="slides" checked="checked" id="slide-1"/>
+            {renderRadioInputs}
+            <ul class="carousel__slides">
+              {renderCarouselSlides}
+            </ul>
+            <ul class="carousel__thumbnails">
+              {renderCarouselThumbnails}
+            </ul>
+          </div>
+        </div>
+
+      </section>
+      
     )
   })
 
@@ -95,7 +148,7 @@ function Crops() {
         {renderTabs}
       </ul>
       </nav>
-      <div className="container">
+      <div className="whole-container">
         
         {/* <div id="home" class="page">Home</div>
         <div id="services" class="page">Services</div>
@@ -108,3 +161,105 @@ function Crops() {
 }
 
 export default Crops
+
+
+{/* <section>
+    <div class="container">
+        <div class="carousel">
+            <input type="radio" name="slides" checked="checked" id="slide-1">
+            <input type="radio" name="slides" id="slide-2">
+            <input type="radio" name="slides" id="slide-3">
+            <input type="radio" name="slides" id="slide-4">
+            <input type="radio" name="slides" id="slide-5">
+            <input type="radio" name="slides" id="slide-6">
+            <ul class="carousel__slides">
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="https://picsum.photos/id/1041/800/450" alt="">
+                        </div>
+                        <figcaption>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            <span class="credit">Photo: Tim Marshall</span>
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="https://picsum.photos/id/1043/800/450" alt="">
+                        </div>
+                        <figcaption>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            <span class="credit">Photo: Christian Joudrey</span>                            
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="https://picsum.photos/id/1044/800/450" alt="">
+                        </div>
+                        <figcaption>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            <span class="credit">Photo: Steve Carter</span>                            
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="https://picsum.photos/id/1045/800/450" alt="">
+                        </div>
+                        <figcaption>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            <span class="credit">Photo: Aleksandra Boguslawska</span>                            
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="https://picsum.photos/id/1049/800/450" alt="">
+                        </div>
+                        <figcaption>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            <span class="credit">Photo: Rosan Harmens</span>                            
+                        </figcaption>
+                    </figure>
+                </li>
+                <li class="carousel__slide">
+                    <figure>
+                        <div>
+                            <img src="https://picsum.photos/id/1052/800/450" alt="">
+                        </div>
+                        <figcaption>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            <span class="credit">Photo: Annie Spratt</span>                            
+                        </figcaption>
+                    </figure>
+                </li>
+            </ul>    
+            <ul class="carousel__thumbnails">
+                <li>
+                    <label for="slide-1"><img src="https://picsum.photos/id/1041/150/150" alt=""></label>
+                </li>
+                <li>
+                    <label for="slide-2"><img src="https://picsum.photos/id/1043/150/150" alt=""></label>
+                </li>
+                <li>
+                    <label for="slide-3"><img src="https://picsum.photos/id/1044/150/150" alt=""></label>
+                </li>
+                <li>
+                    <label for="slide-4"><img src="https://picsum.photos/id/1045/150/150" alt=""></label>
+                </li>
+                <li>
+                    <label for="slide-5"><img src="https://picsum.photos/id/1049/150/150" alt=""></label>
+                </li>
+                <li>
+                    <label for="slide-6"><img src="https://picsum.photos/id/1052/150/150" alt=""></label>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section> */}
